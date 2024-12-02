27b696d7021488518dc7b80cc9cb3f0add68378f
Tadeu <tadeubranconeto@gmail.com>
2024-12-02 15:38:29 -0300

    feat: fix to theme

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -36,7 +36,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <body className={theme}>
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-            <div className={theme}>
+            <div>
                 {children}
             </div>
         </ThemeContext.Provider>
